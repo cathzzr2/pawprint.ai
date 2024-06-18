a38fc9042054aa9c4b05d3472971a5f399a04c15
nononoononon <118186497+nononoononon@users.noreply.github.com>
2024-06-18 15:12:09 -0400

    add new endpoint for Media Quality Service

--- src/main/java/org/abx/virturalpet/controller/MediaQualityServiceController.java
+++ src/main/java/org/abx/virturalpet/controller/MediaQualityServiceController.java
@@ -4,7 +4,11 @@ import org.abx.virturalpet.dto.ImprovePhotoDto;
 import org.abx.virturalpet.service.MediaQualityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MediaQualityServiceController {
