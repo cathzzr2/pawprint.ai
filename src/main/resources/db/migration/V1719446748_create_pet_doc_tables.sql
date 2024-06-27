47f548658d1c45c0a7a1bee7cefe3e326d231315
“zhuoning” <zl4490@nyu.edu>
2024-06-26 22:51:31 -0400

    change schema

---
+++ src/main/resources/db/migration/V1719446748_create_pet_doc_tables.sql
@@ -0,0 +1,26 @@
+CREATE SCHEMA IF NOT EXISTS virtual_pet_schema;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
+CREATE TABLE IF NOT EXISTS virtual_pet_schema.pets (
+    pet_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    pet_name VARCHAR(50) NOT NULL,
+    pet_type VARCHAR(50) NOT NULL,
+    pet_breed VARCHAR(50) NOT NULL,
+    pet_age SMALLINT NOT NULL,
+    pet_birthdate DATE NOT NULL,
+    pet_gender VARCHAR(10) NOT NULL,
+    pet_color VARCHAR(50) NOT NULL,
+    owner_id UUID UNIQUE NOT NULL
+    );
+
+CREATE INDEX IF NOT EXISTS users_pet_name_idx ON virtual_pet_schema.pets (pet_name);
+
+CREATE TABLE IF NOT EXISTS virtual_pet_schema.users (
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_name VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    pet_ids INT[],
+    last_active TIMESTAMP DEFAULT current_timestamp
+    );
