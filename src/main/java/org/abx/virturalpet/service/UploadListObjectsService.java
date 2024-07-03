0090305138a43106fa46303fe6b552bb896f0c21
nononoononon <118186497+nononoononon@users.noreply.github.com>
2024-07-03 19:12:16 -0400

    change to use uploadservicedto

---
+++ src/main/java/org/abx/virturalpet/service/UploadListObjectsService.java
@@ -0,0 +1,119 @@
+package org.abx.virturalpet.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import org.abx.virturalpet.dto.ImmutableUploadServiceDto;
+import org.abx.virturalpet.dto.UploadServiceDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+@Service
+public class UploadListObjectsService {
+    private final S3Client s3Client;
+
+    @Autowired
+    public UploadListObjectsService(S3Client s3Client) {
+        this.s3Client = s3Client;
+        // this.photoRepository = photoRepository;
+    }
+
+    public UploadServiceDto listObject(String bucketName, String prefix) {
+        ListObjectsV2Request listObjectsV2Request = ListObjectsV2Request.builder()
+                .bucket(bucketName)
+                .prefix(prefix)
+                .maxKeys(1) // only get one object
+                .build();
+
+        ListObjectsV2Response listObjectsV2Response = null;
+        try {
+            listObjectsV2Response = s3Client.listObjectsV2(listObjectsV2Request);
+        } catch (S3Exception e) {
+            // System.err.println(e.awsErrorDetails().errorMessage());
+        }
+        if (listObjectsV2Response == null || listObjectsV2Response.contents().isEmpty()) {
+            return null;
+        }
+
+        return listObjectsV2Response.contents().stream()
+                .map(s3Object -> {
+                    String key = s3Object.key();
+                    String fileName = extractFileNameFromKey(key);
+                    Map<String, String> metadata = extractObjectMetadata(bucketName, key);
+                    return ImmutableUploadServiceDto.builder()
+                            .s3Key(key)
+                            .fileName(fileName)
+                            .userId("") // repalce with photoModel.getUserId()
+                            .photoId(UUID.randomUUID().toString()) // repalce with photoModel.getPhotoID()
+                            .timestamp(LocalDateTime.now().toString()) // repalce with photoModel.getUploadTime()
+                            .metadata(metadata.toString())
+                            .build();
+                })
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<UploadServiceDto> listObjectsWithPagination(String bucketName, String prefix, int offset, int limit) {
+        ListObjectsV2Request listObjectsV2Request = ListObjectsV2Request.builder()
+                .bucket(bucketName)
+                .prefix(prefix)
+                .maxKeys(limit)
+                .build();
+
+        ListObjectsV2Response listObjectsV2Response = null;
+        try {
+            listObjectsV2Response = s3Client.listObjectsV2(listObjectsV2Request);
+        } catch (S3Exception e) {
+            // System.err.println(e.awsErrorDetails().errorMessage());
+        }
+
+        if (listObjectsV2Response == null || listObjectsV2Response.contents().isEmpty()) {
+            return List.of(); // return empty list
+        }
+
+        return listObjectsV2Response.contents().stream()
+                .skip(offset)
+                .map(s3Object -> {
+                    String key = s3Object.key();
+                    String fileName = extractFileNameFromKey(key);
+                    Map<String, String> metadata = extractObjectMetadata(bucketName, key);
+                    return ImmutableUploadServiceDto.builder()
+                            .s3Key(key)
+                            .fileName(fileName)
+                            .userId("") // repalce with photoModel.getUserId()
+                            .photoId(UUID.randomUUID().toString()) // repalce with photoModel.getPhotoID()
+                            .timestamp(LocalDateTime.now().toString()) // repalce with photoModel.getUploadTime()
+                            .metadata(metadata.toString())
+                            .build();
+                })
+                .collect(Collectors.toList());
+    }
+
+    // extract filename from s3 key
+    private String extractFileNameFromKey(String key) {
+        // assume filename is the last part
+        return key.substring(key.lastIndexOf('/') + 1);
+    }
+
+    // extract metadata from s3 key
+    private Map<String, String> extractObjectMetadata(String bucketName, String key) {
+        try {
+            HeadObjectRequest headObjectRequest =
+                    HeadObjectRequest.builder().bucket(bucketName).key(key).build();
+
+            HeadObjectResponse headObjectResponse = s3Client.headObject(headObjectRequest);
+            return headObjectResponse.metadata();
+        } catch (S3Exception e) {
+            // System.err.println(e.awsErrorDetails().errorMessage());
+            return Map.of(); // return null
+        }
+    }
+}
---
+++ src/test/java/org/abx/virturalpet/service/UploadListObjectsServiceTest.java
@@ -0,0 +1,100 @@
+package org.abx.virturalpet.service;
+
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import org.abx.virturalpet.dto.ImmutableUploadServiceDto;
+import org.abx.virturalpet.dto.UploadServiceDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+public class UploadListObjectsServiceTest {
+
+    private final S3Client s3Client = Mockito.mock(S3Client.class);
+    private final UploadListObjectsService uploadListObjectsService = new UploadListObjectsService(s3Client);
+
+    @Test
+    public void testListObject_ok() {
+        S3Object s3Object = S3Object.builder().key("test-key/test-file.txt").build();
+        ListObjectsV2Response listObjectsV2Response =
+                ListObjectsV2Response.builder().contents(s3Object).build();
+        HeadObjectResponse headObjectResponse =
+                HeadObjectResponse.builder().metadata(Map.of("key1", "value1")).build();
+
+        when(s3Client.listObjectsV2(Mockito.any(ListObjectsV2Request.class))).thenReturn(listObjectsV2Response);
+        when(s3Client.headObject(Mockito.any(HeadObjectRequest.class))).thenReturn(headObjectResponse);
+
+        UploadServiceDto actualResult = uploadListObjectsService.listObject("test-bucket", "test-prefix");
+
+        Assertions.assertNotNull(actualResult, "Actual result should not be null");
+
+        UploadServiceDto expectedResult = ImmutableUploadServiceDto.builder()
+                .s3Key("test-key/test-file.txt")
+                .fileName("test-file.txt")
+                .userId("")
+                .photoId(actualResult.getPhotoId())
+                .timestamp(actualResult.getTimestamp())
+                .metadata("{key1=value1}")
+                .build();
+
+        Assertions.assertEquals(expectedResult.getS3Key(), actualResult.getS3Key());
+        Assertions.assertEquals(expectedResult.getFileName(), actualResult.getFileName());
+        Assertions.assertEquals(expectedResult.getMetadata(), actualResult.getMetadata());
+    }
+
+    @Test
+    public void testListObject_notFound() {
+        ListObjectsResponse listObjectsResponse =
+                ListObjectsResponse.builder().contents(Collections.emptyList()).build();
+
+        when(s3Client.listObjects(Mockito.any(ListObjectsRequest.class))).thenReturn(listObjectsResponse);
+
+        UploadServiceDto actualResult = uploadListObjectsService.listObject("test-bucket", "test-prefix");
+
+        Assertions.assertNull(actualResult);
+    }
+
+    @Test
+    public void testListObjectsWithPagination_ok() {
+        S3Object s3Object = S3Object.builder().key("test-key/test-file.txt").build();
+        ListObjectsV2Response listObjectsV2Response =
+                ListObjectsV2Response.builder().contents(s3Object).build();
+        HeadObjectResponse headObjectResponse =
+                HeadObjectResponse.builder().metadata(Map.of("key1", "value1")).build();
+
+        when(s3Client.listObjectsV2(Mockito.any(ListObjectsV2Request.class))).thenReturn(listObjectsV2Response);
+        when(s3Client.headObject(Mockito.any(HeadObjectRequest.class))).thenReturn(headObjectResponse);
+
+        UploadServiceDto expectedResult = ImmutableUploadServiceDto.builder()
+                .s3Key("test-key/test-file.txt")
+                .fileName("test-file.txt")
+                .userId("")
+                .photoId(UUID.randomUUID().toString()) // Replace with actual value
+                .timestamp(LocalDateTime.now().toString())
+                .metadata("{key1=value1}")
+                .build();
+
+        List<UploadServiceDto> actualResults =
+                uploadListObjectsService.listObjectsWithPagination("test-bucket", "test-prefix", 0, 10);
+
+        Assertions.assertEquals(1, actualResults.size());
+        Assertions.assertEquals(expectedResult.getS3Key(), actualResults.get(0).getS3Key());
+        Assertions.assertEquals(
+                expectedResult.getFileName(), actualResults.get(0).getFileName());
+        Assertions.assertEquals(
+                expectedResult.getMetadata(), actualResults.get(0).getMetadata());
+    }
+}
