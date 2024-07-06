b8d9d9defee9000f068c687f2398563eb982a251
nononoononon <118186497+nononoononon@users.noreply.github.com>
2024-07-06 01:29:08 -0400

    add listObjectRequestDto and Uploadlistobjectcontroller

--- src/test/java/org/abx/virturalpet/controller/UploadServiceControllerTest.java
+++ src/test/java/org/abx/virturalpet/controller/UploadServiceControllerTest.java
@@ -1,8 +1,17 @@
 package org.abx.virturalpet.controller;
 
+import static org.mockito.Mockito.when;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Collections;
+import java.util.List;
+import org.abx.virturalpet.dto.ImmutableListObjectsRequestDto;
 import org.abx.virturalpet.dto.ImmutableUploadServiceDto;
+import org.abx.virturalpet.dto.ListObjectsRequestDto;
 import org.abx.virturalpet.dto.UploadServiceDto;
+import org.abx.virturalpet.service.UploadListObjectsService;
 import org.abx.virturalpet.service.UploadService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +29,36 @@ public class UploadServiceControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private UploadService uploadService;
+
+    @MockBean
+    private UploadListObjectsService uploadListObjectsService;
+
+    private ListObjectsRequestDto listObjectsRequestDto;
+    private UploadServiceDto uploadServiceDto;
+
+    @BeforeEach
+    void before() {
+        listObjectsRequestDto = ImmutableListObjectsRequestDto.builder()
+                .buckName("test-bucket")
+                .prefix("test-prefix")
+                .offset(0)
+                .limit(10)
+                .build();
+
+        uploadServiceDto = ImmutableUploadServiceDto.builder()
+                .s3Key("test-key")
+                .fileName("test-file")
+                .userId("test-user")
+                .photoId("test-photo")
+                .timestamp("2023-07-01T12:00:00")
+                .metadata("test-metadata")
+                .build();
+    }
 
     @Test
     public void testUploadMediaRequestSuccess() throws Exception {
@@ -35,7 +72,7 @@ public class UploadServiceControllerTest {
                 .metadata("some metadata")
                 .build();
 
-        Mockito.when(uploadService.uploadMediaRequest(Mockito.anyString(), Mockito.any(byte[].class)))
+        when(uploadService.uploadMediaRequest(Mockito.anyString(), Mockito.any(byte[].class)))
                 .thenReturn(dto);
 
         mockMvc.perform(MockMvcRequestBuilders.multipart("/media/upload")
@@ -68,7 +105,7 @@ public class UploadServiceControllerTest {
         MockMultipartFile file =
                 new MockMultipartFile("file", "test.txt", MediaType.TEXT_PLAIN_VALUE, "Test content".getBytes());
 
-        Mockito.when(uploadService.uploadMediaRequest(Mockito.anyString(), Mockito.any(byte[].class)))
+        when(uploadService.uploadMediaRequest(Mockito.anyString(), Mockito.any(byte[].class)))
                 .thenThrow(new RuntimeException("Runtime Exception"));
 
         mockMvc.perform(MockMvcRequestBuilders.multipart("/media/upload")
@@ -77,5 +114,73 @@ public class UploadServiceControllerTest {
                         .param("timestamp", "2023-01-01T00:00:00Z")
                         .param("metadata", "some metadata"))
                 .andExpect(MockMvcResultMatchers.status().isInternalServerError());
+    }
+
+    @Test
+    public void testListObject() throws Exception {
+        when(uploadListObjectsService.listObject(
+                        listObjectsRequestDto.getBuckName(), listObjectsRequestDto.getPrefix()))
+                .thenReturn(uploadServiceDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/media/upload/list-object")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(listObjectsRequestDto)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.s3_key").value("test-key"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.file_name").value("test-file"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.user_id").value("test-user"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.photo_id").value("test-photo"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").value("2023-07-01T12:00:00"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.metadata").value("test-metadata"));
+    }
+
+    @Test
+    public void testListObjects_NotFound() throws Exception {
+        when(uploadListObjectsService.listObject(
+                        listObjectsRequestDto.getBuckName(), listObjectsRequestDto.getPrefix()))
+                .thenReturn(null);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/media/upload/list-object")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(listObjectsRequestDto)))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    public void testListObjectsWithPagination() throws Exception {
+        List<UploadServiceDto> uploadServiceDtos = Collections.singletonList(uploadServiceDto);
+
+        when(uploadListObjectsService.listObjectsWithPagination(
+                        listObjectsRequestDto.getBuckName(),
+                        listObjectsRequestDto.getPrefix(),
+                        listObjectsRequestDto.getOffset(),
+                        listObjectsRequestDto.getLimit()))
+                .thenReturn(uploadServiceDtos);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/media/upload/list-objects")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(listObjectsRequestDto)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].s3_key").value("test-key"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].file_name").value("test-file"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].user_id").value("test-user"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].photo_id").value("test-photo"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].timestamp").value("2023-07-01T12:00:00"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].metadata").value("test-metadata"));
+    }
+
+    @Test
+    void testListObjectsWithPagination_NotFound() throws Exception {
+        when(uploadListObjectsService.listObjectsWithPagination(
+                        listObjectsRequestDto.getBuckName(),
+                        listObjectsRequestDto.getPrefix(),
+                        listObjectsRequestDto.getOffset(),
+                        listObjectsRequestDto.getLimit()))
+                .thenReturn(Collections.emptyList());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/media/upload/list-objects")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(listObjectsRequestDto)))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 }
