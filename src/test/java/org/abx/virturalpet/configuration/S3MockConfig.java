9cbe3c221f4fdf58bd999bf728169f786bf3f85f
will_bit <williamc.c999@gmail.com>
2024-07-15 21:39:43 +0800

    update

--- src/test/java/org/abx/virturalpet/configuration/S3MockConfig.java
+++ src/test/java/org/abx/virturalpet/configuration/S3MockConfig.java
@@ -1,17 +1,21 @@
 package org.abx.virturalpet.configuration;
 
-import com.adobe.testing.s3mock.junit5.S3MockExtension;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
-import java.net.URI;
-
-@ExtendWith(S3MockExtension.class)
 @TestConfiguration
 public class S3MockConfig {
 
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder().region(Region.US_EAST_1).build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder().region(Region.US_EAST_1).build();
+    }
 }
--- src/test/java/org/abx/virturalpet/repository/DeleteObjectTest.java
+++ src/test/java/org/abx/virturalpet/repository/DeleteObjectTest.java
@@ -2,10 +2,10 @@ package org.abx.virturalpet.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.adobe.testing.s3mock.junit5.S3MockExtension;
 import com.adobe.testing.s3mock.testcontainers.S3MockContainer;
 import java.net.URI;
 import java.util.List;
+import org.abx.virturalpet.configuration.S3MockConfig;
 import org.abx.virturalpet.exception.S3DeleteException;
 import org.abx.virturalpet.service.S3Service;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,74 +13,74 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.http.SdkHttpConfigurationOption;
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3Configuration;
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 import software.amazon.awssdk.utils.AttributeMap;
 
-@ExtendWith(S3MockExtension.class)
 @Testcontainers
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = S3MockConfig.class)
 public class DeleteObjectTest {
+    private final Logger logger = LoggerFactory.getLogger(DeleteObjectTest.class);
     private final String testBucketName = "test-bucket";
     private final String testObjectKey = "test-object-key";
-    private S3MockContainer s3MockContainer = new S3MockContainer("latest");
+    private final S3MockContainer s3MockContainer = new S3MockContainer("latest");
+    private S3Client s3Client;
     private S3Service s3Service;
-    private Logger logger = LoggerFactory.getLogger(DeleteObjectTest.class);
 
+    @BeforeEach
+    public void before() {
+        s3MockContainer.start();
+        var endpoint = s3MockContainer.getHttpsEndpoint();
+        var serviceConfig =
+                S3Configuration.builder().pathStyleAccessEnabled(true).build();
+        var httpClient = UrlConnectionHttpClient.builder()
+                .buildWithDefaults(AttributeMap.builder()
+                        .put(SdkHttpConfigurationOption.TRUST_ALL_CERTIFICATES, Boolean.TRUE)
+                        .build());
 
-//    @BeforeEach
-//    public void before() {
-//        s3MockContainer.start();
-////        var endpoint = s3MockContainer.getHttpsEndpoint();
-////        var serviceConfig =
-////                S3Configuration.builder().pathStyleAccessEnabled(true).build();
-////        var httpClient = UrlConnectionHttpClient.builder()
-////                .buildWithDefaults(AttributeMap.builder()
-////                        .put(SdkHttpConfigurationOption.TRUST_ALL_CERTIFICATES, Boolean.TRUE)
-////                        .build());
-////        s3Client = S3Client.builder()
-////                .credentialsProvider(
-////                        StaticCredentialsProvider.create(AwsBasicCredentials.create("dummy-key", "dummy-secret")))
-////                .endpointOverride(URI.create(endpoint))
-////                .serviceConfiguration(serviceConfig)
-////                .httpClient(httpClient)
-////                .build();
-//        extracted();
-//    }
-//
-//    private void extracted() {
-//        s3Service = new S3Service(s3Client);
-//
-//        try {
-//            CreateBucketRequest createBucketRequest =
-//                    CreateBucketRequest.builder().bucket(testBucketName).build();
-//
-//            s3Client.createBucket(createBucketRequest);
-//            logger.info("Bucket created successfully");
-//
-//        } catch (S3Exception e) {
-//            logger.error("Error creating bucket", e);
-//        }
-//    }
+        s3Client = S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.US_EAST_1)
+                .httpClient(httpClient)
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(AwsBasicCredentials.create("dummy-key", "dummy-secret")))
+                .serviceConfiguration(serviceConfig)
+                .build();
+        s3Service = new S3Service(s3Client);
+
+        try {
+            CreateBucketRequest createBucketRequest =
+                    CreateBucketRequest.builder().bucket(testBucketName).build();
+
+            s3Client.createBucket(createBucketRequest);
+            logger.info("Bucket created successfully");
+
+        } catch (S3Exception e) {
+            logger.error("Error creating bucket", e);
+        }
+    }
 
     @Test
-    public void deleteObject(final S3Client s3Client) {
-        s3Client.createBucket(CreateBucketRequest.builder().bucket(testBucketName).build());
-        s3Service = new S3Service(s3Client);
+    public void deleteObject() {
         s3Service.deleteObject(testBucketName, testObjectKey);
         var deletedObject = s3Service.getObject(testBucketName, testObjectKey);
         assertThat(deletedObject).isNull();
     }
 
     @Test
-    public void deleteObjectWithException(final S3Client s3Client) {
+    public void deleteObjectWithException() {
         try {
             s3Service.deleteObject(testBucketName, testObjectKey);
 
@@ -90,7 +90,7 @@ public class DeleteObjectTest {
     }
 
     @Test
-    public void deleteObjects(final S3Client s3Client) {
+    public void deleteObjects() {
         s3Service.deleteObjects(testBucketName, String.valueOf(List.of(testObjectKey)));
         var deletedObject = s3Service.getObject(testBucketName, testObjectKey);
         assertThat(deletedObject).isNull();
