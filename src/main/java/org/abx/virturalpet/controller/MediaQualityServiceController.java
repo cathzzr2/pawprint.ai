0d56aaa1be51982b9aa35cc4bd7f3d82e904ede5
Ivy Lu <wanchinglu0710@gmail.com>
2024-07-15 08:39:08 +0800

    fix checkstyle

--- src/main/java/org/abx/virturalpet/controller/MediaQualityServiceController.java
+++ src/main/java/org/abx/virturalpet/controller/MediaQualityServiceController.java
@@ -1,5 +1,6 @@
 package org.abx.virturalpet.controller;
 
+import java.util.UUID;
 import org.abx.virturalpet.dto.ImprovePhotoJbDto;
 import org.abx.virturalpet.dto.ImprovedPhotoResultDto;
 import org.abx.virturalpet.service.MediaQualityService;
@@ -12,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
 
 @RestController
 public class MediaQualityServiceController {
