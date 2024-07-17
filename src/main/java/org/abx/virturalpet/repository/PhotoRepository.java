15b65dd01c5c72297e397b4311cd1fb66819a548
AdaaaaaZhang <zhangruohan0813@outlook.com>
2024-07-17 09:42:22 -0400

    add test & style check

--- src/main/java/org/abx/virturalpet/repository/PhotoRepository.java
+++ src/main/java/org/abx/virturalpet/repository/PhotoRepository.java
@@ -7,9 +7,9 @@ import org.abx.virturalpet.model.PhotoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhotoRepository extends JpaRepository<PhotoModel, UUID> {
-    Optional<List<PhotoModel>> findByUserId(UUID userId);
+    List<PhotoModel> findByUserId(UUID userId);
 
-    Optional<List<PhotoModel>> findByUploadTime(java.sql.Timestamp lastActive);
+    List<PhotoModel> findByUploadTime(java.sql.Timestamp lastActive);
 
     Optional<PhotoModel> findByS3Key(String s3Key);
 }
