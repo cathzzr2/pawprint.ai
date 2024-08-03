fae9a25459def62e0c3f918c08d055f0d83adaff
Ivy Lu <wanchinglu0710@gmail.com>
2024-08-03 08:44:03 +0800

    repo

--- src/main/java/org/abx/virturalpet/repository/MessageRepository.java
+++ src/main/java/org/abx/virturalpet/repository/MessageRepository.java
@@ -3,14 +3,18 @@ package org.abx.virturalpet.repository;
 import java.util.List;
 import java.util.UUID;
 import org.abx.virturalpet.model.MessageModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends MongoRepository<MessageModel, String> {
+    // Overloaded methods
     List<MessageModel> findByThreadId(UUID threadId);
+    Page<MessageModel> findByThreadId(UUID threadId, Pageable pageable);
 
+    // Non-overloaded methods
     MessageModel findByMessage(String message);
-
     List<MessageModel> findByUserId(UUID userId);
 }
